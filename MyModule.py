e6115f4f4ece977ceff5c7a764e75bf3ffe15173
amatossian <alexandre93@gmail.com>
2017-04-14 11:26:48 +0200

    Convert the age as string in order to be able to print it.

--- MyModule.py
+++ MyModule.py
@@ -1,7 +1,7 @@
 class Animal(object):
     def __init__(self, startname, age):
         self.name = startname #attribute1
-        self.age =  age #attribut2
+        self.age = age #attribut2
     def description(self): #Method1
         print("This is" + self.name)
-        print("She it is  " + self.age)
+        print("She/it is  " + str(self.age))
